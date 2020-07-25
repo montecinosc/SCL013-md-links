07aae1f0ecb3e0230318c2bd78bfdf3d268d6c83
Lilibeth-Gaete <lilibeth.gaete@gmail.com>
2020-07-25 14:38:26 -0400

    Markdown a HTML

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+const fs = require('fs');
+const  jsdom  =  require ( "jsdom" ) ; 
+const  {  JSDOM  }  =  jsdom ;
+const colors = require('colors');
+
+//Lectura RaedMe markdown 
+fs.readFile('README.md','utf8', (err, data)=> {
+    if(err){
+        console.log("NOO un archivo .md")
+    }else
+    {
+        //console.log(data.toString())
+    }
+
+  //Convertir Markdown a HTML
+  const showdown  = require('showdown'),
+  converter = new showdown.Converter(),
+  text      = data.toString(),
+  html      = converter.makeHtml(text);
+ 
+ // Pasa el ReadMe a HTML
+  const myHtml = html;
+  //console.log(myHtml)
+
+  const dom = new JSDOM(myHtml);
+  const test = dom.window.document.querySelector('p').textContent;
+  console.log(test)
+  
+  });
