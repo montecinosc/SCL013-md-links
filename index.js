5bed2a1aeee86db1152d92dc61ff610239fdee86
fer <amontecinoscontreras@gmail.com>
2020-07-28 11:02:17 -0400

    eslint listo

--- index.js
+++ index.js
@@ -1,59 +1,54 @@
-/*module.exports = () => {
-  // ...
-};*/
-
 const fs = require('fs');
-const  jsdom  =  require ( "jsdom" ) ; 
-const  {  JSDOM  }  =  jsdom ;
+
+const jsdom = require('jsdom');
+
+const {
+  JSDOM,
+} = jsdom;
+
 const path = __dirname;
+
 const pathNode = require('path');
+
+const showdown = require('showdown');
 
 // Aroja solo files .md
 fs.readdir(path, (error, files) => {
-  files.forEach(file => {
-  if (file.includes(".md")) {
-  console.log("files", file)
+  files.forEach((file) => {
+    if (file.includes('.md')) {
+      console.log('files', file);
 
-// Lectura RaedMe markdown 
-fs.readFile('README.md','utf8', (err, data)=> {
-    if(err){
-        console.log("Don´t file .md")
-    }else
-    {
-        //console.log(data.toString())
+      // Lectura RaedMe markdown;
+      fs.readFile('README.md', 'utf8', (err, data) => {
+        if (err) {
+          console.log('Don´t file .md');
+        } else {
+          // console.log(data.toString());
+        }
+        // Convertir Markdown a HTML;
+        const converter = new showdown.Converter();
+        const text = data.toString();
+        const html = converter.makeHtml(text);
+
+        // Pasa el ReadMe a HTML;
+        const myHtml = html;
+        // console.log(myHtml);
+
+        // Leer el file HTML y sacar text, link y file;
+        const pathTwo = `${path}${pathNode.sep}${file}`;
+        const dom = new JSDOM(myHtml);
+        const test = dom.window.document.querySelectorAll('a');
+        let addTotal = 0;
+        test.forEach((element) => {
+          addTotal = 1 + addTotal;
+          console.log('----------------------');
+          console.log(element.textContent);
+          console.log(element.href);
+          console.log(pathTwo);
+        });
+        console.log('suma total', addTotal);
+      });
     }
-
-  //Convertir Markdown a HTML
-  const showdown  = require('showdown'),
-  converter = new showdown.Converter(),
-  text      = data.toString(),
-  html      = converter.makeHtml(text);
- 
- // Pasa el ReadMe a HTML
-  const myHtml = html;
-  //console.log(myHtml)
-
-  //Leer el file HTML y sacar text, link y file
-  const pathTwo = `${path}${pathNode.sep}${file}`;
-  const dom = new JSDOM(myHtml);
-  const test = dom.window.document.querySelectorAll('a');
-  let add = 0;
-  test.forEach(element => {
-  addTotal = add + 1
-  console.log("----------------------")
-  console.log(element.textContent)
-  console.log(element.href)
-  console.log(pathTwo)
-
-
-});
-console.log("suma total", suma)
   });
-}
-})
-})
-  console.log("esta es la path", path)
-  
-
-  
-  
+});
+console.log('esta es la path', path);
