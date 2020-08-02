a39b51b0eeaf554365f37c1ef4cf8fca50b6ba3d
Lilibeth-Gaete <lilibeth.gaete@gmail.com>
2020-08-01 20:59:52 -0400

    cambios

--- index.js
+++ index.js
@@ -18,87 +18,104 @@ const fetchUrl = fetch.fetchUrl;
 
 const colors = require('colors');
 
-const index = () => {
-// Aroja solo files .md
-  fs.readdir(path, (error, files) => {
-    files.forEach((file) => {
-      if (file.includes('.md')) {
-        console.log('files', file);  
+const index = (fileIndex) => {
+  const prueba = () => {
+    // Aroja solo files .md
+    fs.readdir(path, (error, files) => {
+      files.forEach((file) => {
+        if (file.includes('.md')) {
+          console.log('files', file);
 
-      // Lectura RaedMe markdown;
-      fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-          console.log('Don´t file .md');
-        } else {
-          // console.log(data.toString());
-        }
-        // Convertir Markdown a HTML;
-        const text = data.toString();
-        const converter = new showdown.Converter();
-        const html = converter.makeHtml(text);
+          // Lectura RaedMe markdown;
+          fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+              console.log('Don´t file .md');
+            } else {
+              // console.log(data.toString());
+            }
+            // Convertir Markdown a HTML;
+            const text = data.toString();
+            const converter = new showdown.Converter();
+            const html = converter.makeHtml(text);
 
-        // Pasa el ReadMe a HTML;
-        const myHtml = html;
-        console.log(myHtml);
-        const getStatus= (url) => {
-          return new Promise((resolve, reject) => {
-            fetchUrl(url, (error, meta) => {
-              if (error) {
-                reject(error);
-              } else {
-                resolve(meta.status);
-                // console.log(meta)
+            // Pasa el ReadMe a HTML;
+            const myHtml = html;
+            console.log(myHtml);
+            const getStatus = (url) => {
+              return new Promise((resolve, reject) => {
+                fetchUrl(url, (error, meta) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(meta.status);
+                    // console.log(meta)
+                  }
+                });
+              });
+            };
+
+            // Truncar texto
+            const truncateText = (text) => {
+              if (text.length > 50) {
+                const textFifty = text.slice(0, 50);
+                return textFifty;
+              } else 
+              {
+                return text;
               }
-            })
-          })
-        };
+            };
 
-        //Truncar texto
-        const only50Caracteres = (text) => {
-          if (text.length > 50) {
-            const text50 = text.slice(0, 50);
-            return text50;
-          } else {
-            return text;
-          }
+            // Leer el file HTML y sacar text, link y file;
+            const pathTwo = `${path}${pathNode.sep}${file}`;
+            const dom = new JSDOM(myHtml);
+            const test = dom.window.document.querySelectorAll('a');
+            let addTotal = 0;
+            test.forEach((element) => {
+              if (element.href.includes('http')) {
+                addTotal = 1 + addTotal;
+                const link = element.href;
+                const textContent = element.textContent;
+                const caracter50 = truncateText(textContent);
+
+                getStatus(link)
+                  .then((res) => {
+                    console.log('----------'.blue);
+                    console.log('text:'.blue, caracter50);
+                    console.log('href:'.blue, link);
+                    console.log('file:'.blue, pathTwo);
+                    console.log('OK ✔'.green, res);
+                  })
+                  .catch(() => {
+                    console.log('----------'.red);
+                    console.log('text:'.red, caracter50);
+                    console.log('href:'.red, link);
+                    console.log('file:'.red, pathTwo);
+                    console.log('error X'.red, err.code, 'error');
+                  });
+              }
+            });
+            console.log('suma total', addTotal);
+          });
         }
-
-        // Leer el file HTML y sacar text, link y file;
-        const pathTwo = `${path}${pathNode.sep}${file}`;
-        const dom = new JSDOM(myHtml);
-        const test = dom.window.document.querySelectorAll('a');
-        let addTotal = 0;
-        test.forEach((element) => {
-          if (element.href.includes('http')){
-            addTotal = 1 + addTotal;
-            const link = element.href;
-            const textContent = element.textContent;
-            const caracter50= only50Caracteres(textContent)
-
-            getStatus(link)
-            .then(res => {
-              console.log('----------'.blue);
-              console.log('text:'.blue, caracter50);
-              console.log('href:'.blue, link);
-              console.log('file:'.blue, pathTwo);
-              console.log('OK ✔'.green, res);
-            })
-            .catch(err => {
-              console.log('----------'.red);
-              console.log('text:'.red, caracter50);
-              console.log('href:'.red, link);
-              console.log('file:'.red, pathTwo);
-              console.log('error X'.red, err.code, 'error');
-            })
-          }
-
-        });
-        console.log('suma total', addTotal);
       });
-    }
-  });
-});
-console.log('esta es la path', path);
+    });
+    console.log('esta es la path', path);
+  };
+  if (fileIndex === '--validate') {
+    console.log('esta validando');
+    prueba(fileIndex);
+  } else if (fileIndex === '--state') {
+    prueba(fileIndex);
+    console.log('estadistica');
+  } else {
+    console.log('Mal escrito,vuelve a escribir');
+  }
+  console.log('salio de la condicion');
+
+  // Aroja solo files .md
+  console.log('esta es la path', path);
 };
 
-module.exports = {index};
+module.exports = {
+  index,
+};
--- mdLink.js
+++ mdLink.js
@@ -1,4 +1,5 @@
 const index = require("./index.js")
 
+const fileIndex= process.argv[2];
 
-index.index()
+index.index(fileIndex)
